d95031eae10a61078f4ac296c30d56696854a032
Sultan999 <32561148+Sultan999@users.noreply.github.com>
2022-01-30 02:06:01 +1100

    Add files via upload

---
+++ Section3_OperatorAndExpressions/Arithmetic/Arithmetic.java
@@ -0,0 +1,16 @@
+package arithmetic;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=14, b=5;
+		
+		float c = (float)a/b;
+		
+		System.out.println(c);
+
+	}
+
+}
